da708060bebcecbdb9342ec8e6f2f7e731c0bc65
Ubaid ur Rehman <77199751+UBAIDSAILANI@users.noreply.github.com>
2022-06-27 23:29:51 +0500

    Add files via upload

---
+++ Assingment 1 chapter 1-2/app.js
@@ -0,0 +1,45 @@
+// Chapter#1
+// Question#1
+alert("welcome Ubaid ur rehman")
+// question#2
+alert("Error! please enter a valid Password")
+// question#3
+alert("Welcome to Js land" + "</br>" +"Happy Coding");
+// question#4
+alert("Welcome to JS land");
+alert("Happy Coding!" + "</br>" + "Prevent this page from additional error");
+// question#5
+alert("Hello... i can run the code in js through console browser");
+// chapter#2
+// question#1
+var userName = "";
+// question#2
+var myName = "Ubaid ur Rehman";
+// question#3
+var message = "Hello world";
+alert(message);
+// question#4
+var name = "Ubaid ur Rehman";
+var age ="15 years old";
+var acc = "certified mobile app developer"
+alert(name + age + acc);
+// question#5
+ var dish = ("Pizza"+ "<br/>"+
+            "Pizz"+
+            "Piz"+
+            "Pi"+
+            "P");
+            alert(dish)
+// question#6
+var email = ("ubaidsailani@gmail.com");
+alert("My email address is " + email);
+// question#7
+var book = "A smarter way to learn JavaScript"
+alert("I am learning the Javascript from " + book);
+// question#8
+var js = "Yah! i can write the HTML content trough javascript" + "<br/>"
+document.write(js);
+// question#9
+var que9 = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(que9);
+document.write(que9)

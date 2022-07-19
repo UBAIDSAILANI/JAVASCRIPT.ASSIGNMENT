0900e2755ff384950fc25548b07c91dd7a8a5f03
Ubaid ur Rehman <77199751+UBAIDSAILANI@users.noreply.github.com>
2022-07-20 00:55:45 +0500

    Add files via upload

---
+++ assingment 7 chapter 21 to 25/app.js
@@ -0,0 +1,10 @@
+// assingment#7
+// Q#1
+var firstName = prompt("Enter your First name");
+var lastName = prompt("Enter your last name");
+var fullName = (firstName + lastName);
+alert("Welcome Mr." + fullName);
+// Q#2
+var favPhone = prompt("What is your favourite Mobile phone");
+var det = ("My Favourite phone is ");
+var result = document.write((det + favPhone));

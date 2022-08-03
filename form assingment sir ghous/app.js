25daba7924c87dc2d5d6900724a73a6cf7d1968d
Ubaid ur Rehman <77199751+UBAIDSAILANI@users.noreply.github.com>
2022-08-03 09:21:07 +0500

    Add files via upload
    This is the form which is using for making form in this we are using setTime out etc....

---
+++ form assingment sir ghous/app.js
@@ -0,0 +1,46 @@
+// function register(){
+// event.preventDefault()
+// var username = document.getElementById("username");
+// var email = document.getElementById("email");
+// var password = document.getElementById("password");
+// var alert = document.getElementById("alert");
+// var loader = document.getElementById("loader")
+
+// loader.style.display = "block"
+
+// setTimeout(function(){
+//     loader.style.display = "none"
+// if(password.value.length > 7){
+//     alert.innerHTML = "successfully registered"
+//     alert.style.display = "block"
+//     setTimeout(function (){
+//         alert.style.display = "none"
+        
+//     }, 2000);
+//     console.log(username.value)
+//     console.log(email.value)
+//     console.log(password.value)
+
+//     username.value = "";
+//     email.value ="";
+//     password.value ="";
+// }
+// else{
+//     danger.innerHTML = "Password should be atleast 8 character"
+//     danger.style.display = "block"
+//     setTimeout(function(){
+//         danger.style.display = 'none'
+//     }, 2000);
+
+//     console.log(username.value)
+//     console.log(email.value)
+//     console.log(password.value)
+
+//     username.value = "";
+//     email.value ="";
+//     password.value ="";
+    
+// }
+// },3000)
+// }
+

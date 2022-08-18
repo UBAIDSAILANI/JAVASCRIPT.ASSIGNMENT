e9ef887ea9ac32d7d6b5e660feb8fe5a073a0d4b
Ubaid ur Rehman <77199751+UBAIDSAILANI@users.noreply.github.com>
2022-08-18 23:39:01 +0500

    Add files via upload

---
+++ Mobile phone search/app.js
@@ -0,0 +1,422 @@
+var mobiles = {
+    apple:{
+        iphone6:{
+            name: "Iphone-6",
+            brand: "Apple",
+            color: ["Silver", "Red", "Black"],
+            camera: "32mpx",
+            sim: "Dual Sim",
+            ram: "4Gb",
+            storage: "32Gb",
+            price: 25000,
+            image: "images/iphone6silver.png",
+        },
+        iphone7:{
+            name: "Iphone-7",
+            brand: "Apple",
+            color: ["Silver", "Red", "Black"],
+            camera: "36mpx",
+            sim: "Dual Sim",
+            ram: "4Gb",
+            storage: "36Gb",
+            price: 30000,
+            image: "images/iphone7black.jpg",
+        },
+        iphone8:{
+            name: "Iphone-8",
+            brand: "Apple",
+            color: ["Silver", "Red", "Black"],
+            camera: "40mpx",
+            sim: "Dual Sim",
+            ram: "8Gb",
+            storage: "56Gb",
+            price: 35000,
+            image: "images/iphone8.jpg",
+        },
+        iphone9:{
+            name: "Iphone-9",
+            brand: "Apple",
+            color: ["Silver", "Red", "Black"],
+            camera: "46mpx",
+            sim: "Dual Sim",
+            ram: "8Gb",
+            storage: "128Gb",
+            price: 44000,
+            image: "images/iphone9.jpg",
+        },
+        iphonex:{
+            name: "Iphone-x",
+            brand: "Apple",
+            image: "images/iphone-X.png",
+            color: ["Silver", "Red", "Black"],
+            camera: "56mpx",
+            sim: "Dual Sim",
+            ram: "8Gb",
+            storage: "256Gb",
+            price: 50000,
+        },
+        iphone11:{
+            name: "Iphone-11",
+            brand: "Apple",
+            color: ["Silver", "Red", "Black"],
+            camera: "40mpx",
+            sim: "Dual Sim",
+            ram: "12Gb",
+            storage: "512Gb",
+            price: 70000,
+            image: "images/iphone11.png",
+        },
+    },
+    samsung:{
+        a32:{
+            name: "Samsung A-32",
+            brand: "Samsung",
+            color: ["Silver", "Red", "Black"],
+            camera: "20mpx",
+            sim: "Dual Sim",
+            ram: "4Gb",
+            storage: "32Gb",
+            price: 15000,
+            image:"images/samsunga32.jpg",
+
+        },
+        a52:{
+            name: "Samsung A-52",
+            brand: "Samsung",
+            color: ["Silver", "Red", "Black"],
+            camera: "24mpx",
+            sim: "Dual Sim",
+            ram: "4Gb",
+            storage: "64Gb",
+            price: 20000,
+            image: "images/samsunga52",
+
+        },
+        a72:{
+            name: "Samsung A-72",
+            brand: "Samsung",
+            color: ["Silver", "Red", "Black"],
+            camera: "20mpx",
+            sim: "Dual Sim",
+            ram: "4Gb",
+            storage: "128Gb",
+            price: 25000,
+            image: "images/samsunga72",
+
+        },
+        note9:{
+            name: "Samsung Note-9",
+            brand: "Samsung",
+            color: ["Silver", "Red", "Black"],
+            camera: "20mpx",
+            sim: "Dual Sim",
+            ram: "4Gb",
+            storage: "64Gb",
+            price: 30000,
+            image: "images/note9.webp",
+
+        },
+        note10:{
+            name: "Samsung Note-10",
+            brand: "Samsung",
+            color: ["Silver", "Red", "Black"],
+            camera: "44mpx",
+            sim: "Dual Sim",
+            ram: "4Gb",
+            storage: "128Gb",
+            price: 50000,
+            image: "images/Note10.webp",
+
+        },
+        note11:{
+            name: "Samsung Note-11",
+            brand: "Samsung",
+            color: ["Silver", "Red", "Black"],
+            camera: "64mpx",
+            sim: "Dual Sim",
+            ram: "8Gb",
+            storage: "512Gb",
+            price: 70000,
+            image: "images/note-8.jpg",
+
+        },
+    },
+    nokia:{
+        nokia3310:{
+        name: "3310",
+        brand: "Nokia",
+        color: ["Silver", "Red", "Black"],
+        camera: "5mp",
+        sim: "Dual Sim",
+        ram: "no",
+        storage: "2Gb",
+        price: 7000,
+        image: "images/nokia3310.webp",
+
+    },
+    nokia2100:{
+        name: "2100",
+        brand: "nokia",
+        color: ["Silver", "Red", "Black"],
+        camera: "no",
+        sim: "single Sim",
+        ram: "no",
+        storage: "no",
+        price: 5000,
+        image: "images/n2100.jpg",
+
+    },
+    nokia2300:{
+        name: "2300",
+        image: "images/n2300.webp",
+        brand: "Nokia",
+        color: ["Silver", "Red", "Black"],
+        camera: "no",
+        sim: "Dual Sim",
+        ram: "no",
+        storage: "no",
+        price: 6000,
+
+    },
+    nokiac1:{
+        name: "C-1",
+        brand: "Nokia",
+        color: ["Silver", "Red", "Black"],
+        camera: "32mp",
+        sim: "Dual Sim",
+        ram: "3Gb",
+        storage: "64Gb",
+        price: 15000,
+        image: "images/nc1.png",
+
+    },
+    nokiac2:{
+        name: "C-2",
+        image: "images/nc2.jpg",
+        brand: "Nokia",
+        color: ["Silver", "Red", "Black"],
+        camera: "32mp",
+        sim: "Dual Sim",
+        ram: "4Gb",
+        storage: "64Gb",
+        price: 20000,
+
+    },
+    nokiac3:{
+        name: "C-3",
+        image: "images/nc3.webp",
+        brand: "Nokia",
+        color: ["Silver", "Red", "Black"],
+        camera: "48mp",
+        sim: "Dual Sim",
+        ram: "6Gb",
+        storage: "128Gb",
+        price: 30000,
+
+    },
+},
+    infinix:{
+        hot9:{
+            name: "Hot-9",
+            brand: "infinix",
+            color: ["Silver", "Red", "Black"],
+            camera: "28Mp",
+            sim: "Dual Sim",
+            ram: "4Gb",
+            storage: "64Gb",
+            price: 25000,
+            image: "images/hot9.jpg"
+    },
+    hot10:{
+        name: "Hot-10",
+        brand: "infinix",
+        color: ["Silver", "Red", "Black"],
+        camera: "36Mp",
+        sim: "Dual Sim",
+        ram: "4Gb",
+        storage: "128Gb",
+        price: 28000,
+        image: "images/hot10.jpg"
+
+
+    },
+    hot11:{
+        name: "Hot-11",
+        brand: "infinix",
+        color: ["Silver", "Red", "Black"],
+        camera: "40Mp",
+        sim: "Dual Sim",
+        ram: "6Gb",
+        storage: "128Gb",
+        price: 32000,
+        image: "images/hot11.jpg"
+
+
+    },
+    hot12:{
+        name: "Hot-12",
+        brand: "infinix",
+        color: ["Silver", "Red", "Black"],
+        camera: "48Mp",
+        sim: "Dual Sim",
+        ram: "6Gb",
+        storage: "128Gb",
+        price: 36000,
+        image: "images/hot12.png"
+
+
+    },
+    zerox:{
+        name: "ZERO-X",
+        brand: "infinix",
+        color: ["Silver", "Red", "Black"],
+        camera: "50mp",
+        sim: "Dual Sim",
+        ram: "6Gb",
+        storage: "256Gb",
+        price: 42000,
+        image: "images/zerox.png",
+
+
+    },
+    gx40:{
+        name: "Gx-40",
+        brand: "infinix",
+        color: ["Silver", "Red", "Black"],
+        camera: "50mp",
+        sim: "Dual Sim",
+        ram: "8Gb",
+        storage: "256Gb",
+        price: 48000,
+        image: "images/note11.webp",
+    },
+    },
+    oppo:{
+        x300:{
+            name: "X-300",
+            brand: "oppo",
+            color: ["Silver", "Red", "Black"],
+            camera: "32mp",
+            sim: "Dual Sim",
+            ram: "4Gb",
+            storage: "64Gb",
+            price: 15000,
+    
+        },
+        x400:{
+            name: "X-400",
+            brand: "oppo",
+            color: ["Silver", "Red", "Black"],
+            camera: "36mp",
+            sim: "Dual Sim",
+            ram: "4Gb",
+            storage: "128Gb",
+            price: 18000,
+    
+        },
+        x500:{
+            name: "X-500",
+            brand: "oppo",
+            color: ["Silver", "Red", "Black"],
+            camera: "40mp",
+            sim: "Dual Sim",
+            ram: "6Gb",
+            storage: "64Gb",
+            price: 22000,
+    
+        },
+        x600:{
+            name: "X-600",
+            brand: "oppo",
+            color: ["Silver", "Red", "Black"],
+            camera: "44mp",
+            sim: "Dual Sim",
+            ram: "6Gb",
+            storage: "128Gb",
+            price: 30000,
+    
+        },
+        x700:{
+            name: "X-700",
+            brand: "oppo",
+            color: ["Silver", "Red", "Black"],
+            camera: "48mp",
+            sim: "Dual Sim",
+            ram: "6Gb",
+            storage: "256Gb",
+            price: 35000,
+    
+        },
+        x800:{
+            name: "X-800",
+            brand: "oppo",
+            color: ["Silver", "Red", "Black"],
+            camera: "50mp",
+            sim: "Dual Sim",
+            ram: "8Gb",
+            storage: "256Gb",
+            image: "",
+            price: 45000,
+    
+        },
+    },
+
+}
+var dropdown = document.getElementById("dropdown");
+var inputs = document.getElementById("mobileInput");
+for(var key in mobiles){
+    var options = document.createElement("option");
+    options.innerHTML = key
+    dropdown.appendChild(options)
+}
+function getValue(){
+    var result = dropdown.options[dropdown.selectedIndex].value;
+    console.log(result)
+
+    var mobileFeatures = inputs.value.toLowerCase();
+    var card = document.getElementById('card');
+    card.innerHTML=`
+    <div class="models">
+    <img src="${mobiles[result][mobileFeatures].image}">
+    <p>Mobile name is: ${mobiles[result][mobileFeatures].name}</p>
+    <p>Mobile Brand is: ${mobiles[result][mobileFeatures].brand}</p>
+    <p>Available Colors are: ${mobiles[result][mobileFeatures].color}</p>
+    <p>Mobile storage is: ${mobiles[result][mobileFeatures].storage}</p>
+    <p>Mobile Ram is: ${mobiles[result][mobileFeatures].ram}</p>
+    <p>Mobile Camera is: ${mobiles[result][mobileFeatures].camera}</p>
+    <p>Sim slots are: ${mobiles[result][mobileFeatures].sim}</p>
+    <p>Mobile Price is: ${mobiles[result][mobileFeatures].price}</p>
+
+
+    
+    
+    </div>`
+}
+
+// function getValue(name, brand, image, color, camera, sim, ram, storage, price){
+// var div = document.createElement("div");
+// var h1 = document.createElement("h1");
+// var h1Text = document.createTextNode(name)
+// h1.appendChild(h1Text);
+// var h2 = document.createElement("h2");
+// var h2Text = document.createTextNode(brand)
+// h2.appendChild(h2Text);
+// var image = document.createElement("img");
+// var h3 = document.createElement("h3");
+// var h3Text = document.createTextNode(color)
+// h3.appendChild(h3Text);
+// var h4 = document.createElement("h4");
+// var h4Text = document.createTextNode(camera)
+// h4.appendChild(h4Text);
+// var sim = document.createElement("p");
+// var ram = document.createElement("h6");
+// var storage = document.createElement("h5");
+// var price = document.createElement("b")
+// div.appendChild(h1)
+// return div;
+// }
+// var result = mobiles["iphones"]["iphonex"]
+
+// var main = document.getElementById("main");
+// main.appendChild(
+// getValue(result.name));
